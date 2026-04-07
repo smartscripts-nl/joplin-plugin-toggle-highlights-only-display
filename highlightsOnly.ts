89d9602c32878ab5d21bf355e57ddcf6384af809
smartscripts-nl <info@smartscripts.nl>
2026-04-07 16:31:50 +0200

    Highlights-only mode: add support for properties in p/li elements

---
+++ highlightsOnly.ts
@@ -0,0 +1,27 @@
+module.exports = {
+    default: function (context) {
+        return {
+            plugin: function (markdownIt) {
+
+                const defaultRender = markdownIt.renderer.render.bind(markdownIt.renderer);
+
+                markdownIt.renderer.render = function (tokens, options, env) {
+                    let html = defaultRender(tokens, options, env);
+
+                    // if no mark elements present, no content change needed:
+                    if (!html.includes('<mark')) {
+                        return html;
+                    }
+
+                    html = html
+                        .replace(/<(p|li)([^>]*)>(.+?)<mark/g, '<$1$2><span class="hide-around-highlight">$3</span><mark')
+                        .replace(/<\/mark>(.+?)<\/(p|li)>/g, '</mark><span class="hide-around-highlight">$1</span></$2>')
+
+                    html = "<div class='highlights-only'>" + html + "</div>";
+
+                    return html;
+                };
+            }
+        };
+    }
+};
