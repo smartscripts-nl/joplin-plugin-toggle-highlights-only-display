f79c03046ba9ac2fce69fe2e0d046cd1561f498c
smartscripts-nl <info@smartscripts.nl>
2026-04-09 17:06:19 +0200

    [hotfix] Mutilation of some html elements

--- src/highlightsOnly.ts
+++ src/highlightsOnly.ts
@@ -8,14 +8,31 @@ module.exports = {
                 markdownIt.renderer.render = function (tokens, options, env) {
                     let html = defaultRender(tokens, options, env);
 
-                    // if no mark elements present, no content change needed:
+                    //* if no mark elements present, no content change needed:
                     if (!html.includes('<mark')) {
                         return html;
                     }
 
-                    html = html
-                        .replace(/<(p|li)([^>]*)>(.+?)<mark/g, '<$1$2><span class="hide-around-highlight">$3</span><mark')
-                        .replace(/<\/mark>(.+?)<\/(p|li)>/g, '</mark><span class="hide-around-highlight">$1</span></$2>')
+                    html = html.replace(
+                        //* don't go past closing tag of p and li elements:
+                        /<(p|li)([^>]*)>([\s\S]*?)<\/\1>/g,
+                        (match, tag, attrs, content) => {
+
+                            let newContent = content
+                                //* BEFORE each <mark>:
+                                .replace(
+                                    /([^<]*?)<mark/g,
+                                    '<span class="hide-around-highlight">$1</span><mark'
+                                )
+                                //* AFTER each </mark>, up to next <mark>:
+                                .replace(
+                                    /<\/mark>([^<]*?)(?=<mark|$)/g,
+                                    '</mark><span class="hide-around-highlight">$1</span>'
+                                );
+
+                            return `<${tag}${attrs}>${newContent}</${tag}>`;
+                        }
+                    );
 
                     html = "<div class='highlights-only'>" + html + "</div>";
 
